d69807684f869f0abfd96d398cb7d6c4f673ecf5
SonaliBedge <bedgesonali@yahoo.com>
2024-05-07 18:29:59 -0700

    Student API-2

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const teachersRouter = require("./routes/teachers");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.set('trust proxy', 1)
+app.set("trust proxy", 1);
 app.use(
   rateLimiter({
     windowMs: 15 * 60 * 1000, //15 minutes
@@ -35,17 +35,16 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-
 // extra packages
 
-// app.get('/', (req, res) => {
-//   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
-// });
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
 
-// routes
+// routesgit 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/students", authenticateUser, studentsRouter);
-app.use("api/v1/teachers", authenticateUser, teachersRouter);
+// app.use("api/v1/teachers", authenticateUser, teachersRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
