fc4695dbe7f585662ad9b1c3b92774d731c2c4d7
SonaliBedge <bedgesonali@yahoo.com>
2024-05-09 14:54:32 -0700

    swagger docs added

--- app.js
+++ app.js
@@ -7,17 +7,22 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 
 //connectDB
 const connectDB = require("./db/connect");
-
 const authenticateUser = require("./middleware/authentication");
+
 //routers
 const authRouter = require("./routes/auth");
 const studentsRouter = require("./routes/students");
-const teachersRouter = require("./routes/teachers");
+// const teachersRouter = require("./routes/teachers");
 
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
@@ -37,11 +42,13 @@ app.use(xss());
 
 // extra packages
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send('<h1>Students API</h1><a href="/api-docs">Documentation</a>');
 });
 
-// routesgit 
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+// routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/students", authenticateUser, studentsRouter);
 // app.use("api/v1/teachers", authenticateUser, teachersRouter);
