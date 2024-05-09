a71caf7e380758bf3bf559cb318d2a780dae20e6
SonaliBedge <bedgesonali@yahoo.com>
2024-05-09 07:43:02 -0700

    swagger docs added

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 require("dotenv").config();
 require("express-async-errors");
+
+//extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
+
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 
 //connectDB
 const connectDB = require("./db/connect");
@@ -10,21 +22,36 @@ const authenticateUser = require("./middleware/authentication");
 //routers
 const authRouter = require("./routes/auth");
 const studentsRouter = require("./routes/students");
+// const teachersRouter = require("./routes/teachers");
 
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, //15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 // extra packages
 
-// app.get('/', (req, res) => {
-//   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
-// });
+app.get("/", (req, res) => {
+  res.send('<h1>Students API</h1><a href="/api-docs">Documentation</a>');
+});
 
-// routes
+app.use("/api-docs", swaggerUI.server, swaggerUI.setup(swaggerDocument));
+
+// routesgit
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/students", authenticateUser, studentsRouter);
+// app.use("api/v1/teachers", authenticateUser, teachersRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
